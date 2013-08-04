47cc594b99ff5dd8b39b6a506e8ded97a0231ab3
Simon Campbell <simon.s.campbell@gmail.com>
2013-08-04 15:58:29 +1200

    - Created a mappable string template and tests.

---
+++ Application.Android/test/nz/ac/waikato/ssc10/text/PlaceholderMapStringTemplateTest.java
@@ -0,0 +1,49 @@
+package nz.ac.waikato.ssc10.text;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Simon
+ * Date: 4/08/13
+ * Time: 3:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlaceholderMapStringTemplateTest {
+    @Test
+    public void testMatches_TwoPlaceholders() throws Exception {
+        assertTrue(new PlaceholderMapStringTemplate("text {0} why u {1}").matches("text which is why u 80!"));
+    }
+
+    @Test
+    public void testMatches_StartTwoPlaceholders() {
+        assertTrue(new PlaceholderMapStringTemplate("{0}{1} this is stupid").matches(" this is stupid"));
+        assertTrue(new PlaceholderMapStringTemplate("{0}{1} this is stupid").matches("the lion king this is stupid"));
+    }
+
+    @Test
+    public void testMatches_ExampleNavigationStrings() {
+        assertTrue(new PlaceholderMapStringTemplate("take me to {Destination} near {Near}").matches("take me to Silverdale Road near the Fish and Chip shop"));
+        assertTrue(new PlaceholderMapStringTemplate("take me to {Destination} near {Near} on {Street}").matches("take me to the two dollar shop near the Fish and Chip shop on Silverdale Road"));
+    }
+
+    @Test
+    public void testVariables_TwoPlaceholders() {
+        Map<String, String> variables =
+                new PlaceholderMapStringTemplate("text {two} why u {three} more")
+                        .variables("text which is why u 80! more");
+
+        assertTrue("which is".equals(variables.get("{two}")));
+        assertTrue("80!".equals(variables.get("{three}")));
+
+        new PlaceholderMapStringTemplate("text {two} why u {three}")
+                .variables("text which is why u 80!");
+
+        assertTrue("which is".equals(variables.get("{two}")));
+        assertTrue("80!".equals(variables.get("{three}")));
+    }
+}
